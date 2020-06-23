fd6455b8e664a6c849f9f88450d8b1c161ecd563
Robert Sösemann <robert.soesemann@logicline.de>
2020-06-23 12:10:47 +0200

    Minor changes with Anmol

--- scripts/createPackage.sh
+++ scripts/createPackage.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 source `dirname $0`/config.sh
 
 execute() {
@@ -12,12 +12,14 @@ echo "Create new package version"
 PACKAGE_VERSION="$(execute sfdx force:package:version:create -p $PACKAGENAME -v $DEV_HUB_ALIAS -x -w 10 --json | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
 echo $PACKAGE_VERSION
 
-if [ "$secrets.QA_URL" ]; then
+sfdx force:package:version:promote -p $PACKAGE_NAME -v $DEV_HUB_ALIAS
+
+if [ $secrets.QA_URL ]; then
   echo "Authenticate and install in QA Org"
 
   echo $secrets.QA_URL > qaURLFile
   sfdx force:auth:sfdxurl:store -f qaURLFile -a $QA_ORG_ALIAS
   rm qaURLFile
 
-  sfdx force:package:install -p $PACKAGE_VERSION -u $QA_ORG_ALIAS -b 3 -w 10 -r
+  sfdx force:package:install -p $PACKAGE_VERSION -u $QA_ORG_ALIAS -b 10 -w 10 -r
 fi
