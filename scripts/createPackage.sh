2f8359bca4c22f3f32b1e1433b45e56ee54993ff
Robert Sösemann <robert.soesemann@logicline.de>
2020-06-23 11:05:09 +0200

    Minor fixes before initial Unlocked Package version creation

--- scripts/createPackage.sh
+++ scripts/createPackage.sh
@@ -9,7 +9,7 @@ echo "List existing package versions"
 sfdx force:package:version:list -p apex-domainbuilder -v DevHubPrivate
 
 echo "Create new package version"
-PACKAGE_VERSION="$(execute sfdx force:package:version:create --package $PACKAGENAME --installationkeybypass --wait 10 --json | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
+PACKAGE_VERSION="$(execute sfdx force:package:version:create -p $PACKAGENAME -v DevHubPrivate -x -w 10 --json | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
 echo $PACKAGE_VERSION
 
 if [ "$QA_URL" ]; then
@@ -19,5 +19,5 @@ if [ "$QA_URL" ]; then
   sfdx force:auth:sfdxurl:store -f qaURLFile -a $QA_ORG_ALIAS
   rm qaURLFile
 
-  sfdx force:package:install -p $PACKAGE_VERSION -u $QA_ORG_ALIAS --publishwait 3 --wait 10 -r
+  sfdx force:package:install -p $PACKAGE_VERSION -u $QA_ORG_ALIAS -b 3 -w 10 -r
 fi
