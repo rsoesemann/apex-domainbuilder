2ccda2aaa33f9e87c7116568f48ea82b0dd9f544
rsosemann <robert@soesemann.com>
2021-07-16 13:12:25 +0200

    .

--- scripts/createScratchOrg.sh
+++ scripts/createScratchOrg.sh
@@ -27,7 +27,7 @@ echo "Make sure Org user is english"
 sfdx force:data:record:update -s User -w "Name='User User'" -v "Languagelocalekey=en_US"
 
 echo "Running apex tests"
-execute sfdx force:apex:test:run -l RunLocalTests -w 30
+execute sfdx force:apex:test:run -l RunLocalTests --synchronous
 
 if [ -f "package.json" ]; then
   echo "Running jest tests"
